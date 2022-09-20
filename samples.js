43386e4559580b5c66a609897632627df29c27c7
Bhumika Brahmbhatt <bhumi3107.bb@gmail.com>
2022-09-20 23:28:59 +0200

    final change

--- samples.js
+++ samples.js
@@ -1,67 +1,272 @@
 export const samples = {
-    "agents_log": {
-         "A":[],
-         "B":[
-            {
-               "date_time":"2022-05-05 14:23:45:579726",
-               "metric_str":"content_trust.direct_experience",
-               "other_agent":"A",
-               "resource_id":"http://example.com/Redecentralization_of_the_Web",
-               "trust_value":"0.0"
-            },
-            {
-               "date_time":"2022-05-05 14:23:45:588587",
-               "metric_str":"content_trust.recommendation",
-               "other_agent":"A",
-               "resource_id":"http://example.com/Redecentralization_of_the_Web",
-               "trust_value":"None"
-            },
-            {
-               "date_time":"2022-05-05 14:23:45:599123",
-               "metric_str":"content_trust.popularity",
-               "other_agent":"A",
-               "resource_id":"http://example.com/Redecentralization_of_the_Web",
-               "trust_value":"0.5"
-            },
-            {
-               "date_time":"2022-05-05 14:23:45:618727",
-               "metric_str":"content_trust.direct_experience",
-               "other_agent":"A",
-               "resource_id":"http://example.com/Web_of_Things",
-               "trust_value":"None"
-            },
-            {
-               "date_time":"2022-05-05 14:23:45:626127",
-               "metric_str":"content_trust.recommendation",
-               "other_agent":"A",
-               "resource_id":"http://example.com/Web_of_Things",
-               "trust_value":"None"
-            },
-            {
-               "date_time":"2022-05-05 14:23:45:635072",
-               "metric_str":"content_trust.popularity",
-               "other_agent":"A",
-               "resource_id":"http://example.com/Web_of_Things",
-               "trust_value":"0.5"
-            }
-         ],
-         "C":[],
-         "D":[]
-      },
-     "trustLogDict": [
-         {
-            "date_time":"2022-05-05 14:23:45:601913",
-            "agent":"B",
-            "other_agent":"A",
-            "resource_id":"http://example.com/Redecentralization_of_the_Web",
-            "trust_value":"0.25"
-         },
-         {
-            "date_time":"2022-05-05 14:23:45:639594",
-            "agent":"B",
-            "other_agent":"A",
-            "resource_id":"http://example.com/Web_of_Things",
-            "trust_value":"0.5"
-         }
-     ]
-};
+   "scenarioName": "Basic Topic Scenario",
+   "supervisorAmount": 1,
+   "atlasTimes": {},
+   "trustLog": [
+     {
+       "agent": "B",
+       "date_time": "2022-09-02 12:50:03:409690",
+       "exec_time": "0.05721092224121094",
+       "other_agent": "A",
+       "resource_id": "http://example.com/Redecentralization_of_the_Web",
+       "trust_value": "0.3333333333333333"
+     },
+     {
+       "agent": "B",
+       "date_time": "2022-09-02 12:50:03:480482",
+       "exec_time": "0.014410972595214844",
+       "other_agent": "A",
+       "resource_id": "http://example.com/Web_of_Things",
+       "trust_value": "0.3055555555555555"
+     },
+     {
+       "agent": "B",
+       "date_time": "2022-09-02 12:50:03:511836",
+       "exec_time": "0.01662898063659668",
+       "other_agent": "A",
+       "resource_id": "http://example.com/Web_Assembly",
+       "trust_value": "0.6265432098765432"
+     },
+     {
+       "agent": "B",
+       "date_time": "2022-09-02 12:50:03:541170",
+       "exec_time": "0.015843868255615234",
+       "other_agent": "C",
+       "resource_id": "http://example.com/Semantic_Web_and_Linked_Open_Data",
+       "trust_value": "0.6666666666666666"
+     },
+     {
+       "agent": "B",
+       "date_time": "2022-09-02 12:50:03:593304",
+       "exec_time": "0.0197141170501709",
+       "other_agent": "C",
+       "resource_id": "http://example.com/Redecentralization_of_the_Web",
+       "trust_value": "0.5625"
+     },
+     {
+       "agent": "B",
+       "date_time": "2022-09-02 12:50:03:658985",
+       "exec_time": "0.018537044525146484",
+       "other_agent": "C",
+       "resource_id": "http://example.com/Web-based_learning",
+       "trust_value": "0.6921296296296297"
+     }
+   ],
+   "agentsLog": {
+     "A": [],
+     "B": [
+       {
+         "date_time": "2022-09-02 12:50:03:352166",
+         "metric_str": "content_trust.authority",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Redecentralization_of_the_Web",
+         "trust_value": "None"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:353313",
+         "metric_str": "content_trust.topic",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Redecentralization_of_the_Web",
+         "trust_value": "0.5"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:357232",
+         "metric_str": "content_trust.direct_experience",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Redecentralization_of_the_Web",
+         "trust_value": "0.0"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:383781",
+         "metric_str": "content_trust.recommendation",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Redecentralization_of_the_Web",
+         "trust_value": "None"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:408399",
+         "metric_str": "content_trust.popularity",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Redecentralization_of_the_Web",
+         "trust_value": "0.5"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:465627",
+         "metric_str": "content_trust.authority",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Web_of_Things",
+         "trust_value": "None"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:467368",
+         "metric_str": "content_trust.topic",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Web_of_Things",
+         "trust_value": "0.41666666666666663"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:468558",
+         "metric_str": "content_trust.direct_experience",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Web_of_Things",
+         "trust_value": "0.0"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:472923",
+         "metric_str": "content_trust.recommendation",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Web_of_Things",
+         "trust_value": "None"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:479085",
+         "metric_str": "content_trust.popularity",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Web_of_Things",
+         "trust_value": "0.5"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:494866",
+         "metric_str": "content_trust.authority",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Web_Assembly",
+         "trust_value": "None"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:496182",
+         "metric_str": "content_trust.topic",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Web_Assembly",
+         "trust_value": "0.3796296296296296"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:497761",
+         "metric_str": "content_trust.direct_experience",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Web_Assembly",
+         "trust_value": "1.0"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:503006",
+         "metric_str": "content_trust.recommendation",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Web_Assembly",
+         "trust_value": "None"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:510528",
+         "metric_str": "content_trust.popularity",
+         "other_agent": "A",
+         "resource_id": "http://example.com/Web_Assembly",
+         "trust_value": "0.5"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:524669",
+         "metric_str": "content_trust.authority",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Semantic_Web_and_Linked_Open_Data",
+         "trust_value": "1.0"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:525951",
+         "metric_str": "content_trust.topic",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Semantic_Web_and_Linked_Open_Data",
+         "trust_value": "0.5"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:527196",
+         "metric_str": "content_trust.direct_experience",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Semantic_Web_and_Linked_Open_Data",
+         "trust_value": "None"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:533231",
+         "metric_str": "content_trust.recommendation",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Semantic_Web_and_Linked_Open_Data",
+         "trust_value": "None"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:539092",
+         "metric_str": "content_trust.popularity",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Semantic_Web_and_Linked_Open_Data",
+         "trust_value": "0.5"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:573185",
+         "metric_str": "content_trust.authority",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Redecentralization_of_the_Web",
+         "trust_value": "1.0"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:574678",
+         "metric_str": "content_trust.topic",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Redecentralization_of_the_Web",
+         "trust_value": "0.5833333333333333"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:576589",
+         "metric_str": "content_trust.direct_experience",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Redecentralization_of_the_Web",
+         "trust_value": "0.16666666666666666"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:583334",
+         "metric_str": "content_trust.recommendation",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Redecentralization_of_the_Web",
+         "trust_value": "None"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:592027",
+         "metric_str": "content_trust.popularity",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Redecentralization_of_the_Web",
+         "trust_value": "0.5"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:640047",
+         "metric_str": "content_trust.authority",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Web-based_learning",
+         "trust_value": "1.0"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:641954",
+         "metric_str": "content_trust.topic",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Web-based_learning",
+         "trust_value": "0.5763888888888888"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:643888",
+         "metric_str": "content_trust.direct_experience",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Web-based_learning",
+         "trust_value": "None"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:650663",
+         "metric_str": "content_trust.recommendation",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Web-based_learning",
+         "trust_value": "None"
+       },
+       {
+         "date_time": "2022-09-02 12:50:03:657149",
+         "metric_str": "content_trust.popularity",
+         "other_agent": "C",
+         "resource_id": "http://example.com/Web-based_learning",
+         "trust_value": "0.5"
+       }
+     ],
+     "C": [],
+     "D": []
+   }
+ };
